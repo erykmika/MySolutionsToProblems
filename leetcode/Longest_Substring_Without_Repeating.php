3ca978bda3be84d1bcd1b8460ed0163b580792f4
Eryk Mika <erykmika108@gmail.com>
2023-08-02 10:45:08 +0200

    Optimize leetcode solution

--- leetcode/Longest_Substring_Without_Repeating.php
+++ leetcode/Longest_Substring_Without_Repeating.php
@@ -16,29 +16,26 @@ function lengthOfLongestSubstring($s) {
         return 0;
     }
 
-    // Convert a string into an array of characters
-    $arr = str_split($s);
-
     // Create a sliding window and find the longest substring matching the condition
     $current_len = 0;
     $max_len = 0;
 
-    $char_count = count($arr);
+    $char_count = strlen($s);
     $hash_table = [];
 
     $left_index = 0;
     $right_index = 0;
 
     while ($left_index < $char_count && $max_len < $char_count - $left_index) {
-        if (!empty($hash_table[$arr[$right_index]])) {
-            $hash_table[$arr[$left_index]] = false;
+        if (!empty($hash_table[$s[$right_index]])) {
+            $hash_table[$s[$left_index]] = false;
             if ($left_index != $right_index) {
                 $left_index++;
                 $current_len--;
             }
         } else {
             $current_len++;
-            $hash_table[$arr[$right_index]] = true;
+            $hash_table[$s[$right_index]] = true;
             $right_index++;
             if ($current_len > $max_len) {
                 $max_len = $current_len;
